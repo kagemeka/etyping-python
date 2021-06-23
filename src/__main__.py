b9d4d8de835d6bee9b9811ab99622a63821490cf
kagemeka <kagemeka1@gmail.com>
2021-06-24 08:14:22 +0900

    enhance: open the typing content (#13)

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,21 @@ import selenium
 from selenium.webdriver import(
   Firefox,
 )
+from \
+  selenium.webdriver \
+  .common.by \
+import (
+  By,
+)
+
+from \
+  selenium.webdriver \
+  .common.keys \
+import (
+  Keys,
+)
+import time
+
 
 
 import dataclasses 
@@ -14,7 +29,28 @@ def main():
   driver = Firefox()
   driver.get('https://www.e-typing.ne.jp/')
   print(driver.page_source)
+  id_ = 'level_check_btn'
+  driver.find_element(
+    by=By.ID,
+    value=id_,
+  ).click()
+  driver.find_element(
+    by=By.ID,
+    value=id_,
+  ).click()
+  id_ = 'start_btn'
+  time.sleep(2)
+  driver.switch_to_frame(
+    'typing_content',
+  )
 
+  with open('tmp.html', 'w') as f:
+    f.write(driver.page_source)
+  driver.find_element(
+    by=By.ID,
+    value=id_,
+  ).click()
+  # driver.close()
 
 
 if __name__ == '__main__':
