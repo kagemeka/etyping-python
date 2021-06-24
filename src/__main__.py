70711233638bd290264ef2307e6e40a439fd836e
kagemeka <kagemeka1@gmail.com>
2021-06-25 08:38:50 +0900

    enhance: create a class to play with login (#22)

--- src/__main__.py
+++ src/__main__.py
@@ -26,12 +26,6 @@ class ETypingCfg():
 
 
 
-def login(driver):
-  ... 
-  
-  email = 'kagemeka1@gmail.com'
-  passwd = 'ruruth12'
-  
 
 
 @dataclasses.dataclass
@@ -41,36 +35,202 @@ class AuthCfg():
 
 
 
+@dataclasses.dataclass
+class PlayWithLoginCfg():
+  base_url: str
+  auth: AuthCfg
+
+
+
+
+@dataclasses.dataclass
+class category():
+  ...
+
+
+
+@dataclasses.dataclass
+class LoginFormID():
+  email: str = 'mail'
+  passwd: str = 'password'
+  submit: str = 'login_btn'
+
+
+
+class PlayWithLogin():
+  def __call__(
+    self,
+  ):
+    self.__init_driver()
+    self.__open_website()
+    self.__login()
+    self.__start_up_game()
+    self.__start_game()
+    self.__play()
+    
+  
+
+  def __init__(
+    self,
+    cfg: PlayWithLoginCfg,
+  ):
+    self.__cfg = cfg 
+  
+
+  def __init_driver(
+    self,
+  ):
+    self.__driver = Firefox()
+
+
+  def __login(
+    self,
+  ):
+    auth = self.__cfg.auth
+    driver = self.__driver
+    ids = LoginFormID()
+    driver.find_element(
+      by=By.ID,
+      value=ids.email,
+    ).send_keys(auth.email)
+    driver.find_element(
+      by=By.ID,
+      value=ids.passwd,
+    ).send_keys(auth.passwd)
+    driver.find_element(
+      by=By.ID,
+      value=ids.submit,
+    ).click()
+
+
+
+  def __open_website(
+    self,
+  ):
+    self.__driver.get(
+      self.__cfg.base_url,
+    )
+
+
+  def __start_up_game(
+    self,
+  ):
+    driver = self.__driver
+    id_ = 'level_check_member'
+    driver.find_element(
+      by=By.ID,
+      value=id_,
+    ).find_element(
+      by=By.TAG_NAME,
+      value='a',
+    ).click()
+    time.sleep(1)
+
+
+  def __start_game(
+    self,
+  ):
+    driver = self.__driver
+    driver.switch_to.frame(
+      'typing_content',
+    )
+    driver.find_element(
+      by=By.ID,
+      value='start_btn',
+    ).click()
+    time.sleep(1)
+    game = driver.find_element(
+      by=By.TAG_NAME,
+      value='body',
+    )
+    game.send_keys(Keys.SPACE)
+    time.sleep(1 << 2)
+    self.__game = game
+
+
+  def __play(
+    self,
+  ):
+    for i in range(15):
+      self.__input()
+      time.sleep(2)
+    self.__register()
+
+
+  def __input(
+    self,
+  ):
+    game = self.__game
+    s = game.find_element(
+      by=By.ID,
+      value='sentenceText',
+    ).text
+    game.send_keys(s)
+    print(s)
+
+  
+  def __register(
+    self,
+  ):
+    game = self.__game
+    game.find_element(
+      by=By.ID,
+      value='regist_btn',
+    ).click()
+    time.sleep(1)
+    game.find_element(
+      by=By.ID,
+      value='yesBtn',
+    ).click()
+    time.sleep(1)
+    game.find_element(
+      by=By.ID,
+      value='okBtn',
+    ).click()
+    # if not ok_btn:
+    #   return
+    # ok_btn.click()
+    
+
+
+
+  
+  
+
+
+def login(driver):
+  ... 
+  
+  email = 'kagemeka1@gmail.com'
+  passwd = 'ruruth12'
+  
+
+
+
 def normalize():
   import string
   print(string.ascii_letters)
 
 
 def main():
-  driver = Firefox()
+
   base_url = (
     'https://www.e-typing.ne.jp/'
   )
-  driver.get(base_url)
-
   email = 'kagemeka1@gmail.com'
   passwd = 'ruruth12'
+  auth = AuthCfg(
+    email=email,
+    passwd=passwd,
+  )
+  cfg = PlayWithLoginCfg(
+    base_url=base_url,
+    auth=auth,
+  )
+  play = PlayWithLogin(cfg)
+  play()
   
-  driver.find_element(
-    by=By.ID,
-    value='mail',
-  ).send_keys(email)
-
-  driver.find_element(
-    by=By.ID,
-    value='password',
-  ).send_keys(passwd)
   
-  driver.find_element(
-    by=By.ID,
-    value='login_btn',
-  ).click()
-
   # url = (
   #   f'{base_url}/member/cht.asp?tp=2'
   # )
@@ -86,63 +246,25 @@ def main():
   #   value=id_,
   # ).click()
 
-  id_ = 'level_check_member'
-  driver.find_element(
-    by=By.ID,
-    value=id_,
-  ).find_element(
-    by=By.TAG_NAME,
-    value='a',
-  ).click()
-  time.sleep(1)
 
-
-  driver.switch_to_frame(
-    'typing_content',
-  )
-
-  id_ = 'start_btn'
-  driver.find_element(
-    by=By.ID,
-    value=id_,
-  ).click()
-
-  time.sleep(1)
-  game = driver.find_element(
-    by=By.TAG_NAME,
-    value='body',
-  )
-  game.send_keys(Keys.SPACE)
-  normalize()
-  time.sleep(1 << 2)
-  for i in range(15):
-    # while True:
-    #   txt = driver.find_element(
-    #     by=By.ID,
-    #     value='sentenceText',
-    #   ).find_elements(
-    #     by=By.TAG_NAME,
-    #     value='span',
-    #   )
-    #   print(txt)
-    #   if not txt:
-    #     break
-    #   txt = txt[1].text
-    #   txt = txt[:10]
-    #   txt = txt.replace(
-    #     '␣', ' ',
-    #   )
-    #   game.send_keys(txt)
-
-
-    txt = driver.find_element(
-      by=By.ID,
-      value='sentenceText',
-    ).text
-
-    game.send_keys(txt)
-    print(txt)
-    time.sleep(2)
+  # for i in range(15):
+  #   # while True:
+  #   #   txt = driver.find_element(
+  #   #     by=By.ID,
+  #   #     value='sentenceText',
+  #   #   ).find_elements(
+  #   #     by=By.TAG_NAME,
+  #   #     value='span',
+  #   #   )
+  #   #   print(txt)
+  #   #   if not txt:
+  #   #     break
+  #   #   txt = txt[1].text
+  #   #   txt = txt[:10]
+  #   #   txt = txt.replace(
+  #   #     '␣', ' ',
+  #   #   )
+  #   #   game.send_keys(txt)
 
 
 
