ac82f4f7847e352d613663e0963b91c1231c2dd9
kagemeka <kagemeka1@gmail.com>
2021-06-27 16:29:04 +0900

    enhance: refactor (#27)

---
+++ src/lib/__init__.py
@@ -0,0 +1,22 @@
+from .login import (
+  AuthCfg,
+  Login,
+)
+from .play import (
+  Play,
+  PlayCfg,
+)
+from .register import (
+  Register,
+)
+from .yml import (
+  Load as LoadYml,
+)
+from .json import (
+  Load as LoadJson,
+)
+
+from .etyping import (
+  ETyping,
+  ETypingCfg,
+)
